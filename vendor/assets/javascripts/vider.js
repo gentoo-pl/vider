f2ee13f152e65461eb71101c24a4e275b863b9a1
gentoo-pl <herman.smietnik@gmail.com>
2015-07-15 09:54:13 +0200

    fixbug in js

--- vendor/assets/javascripts/vider.js
+++ vendor/assets/javascripts/vider.js
@@ -1,7 +1,6 @@
 $(function(){
-  $('#vider-element').each(function( {
-    var $videBg = $(this).attr('data-vide-bg');
-    // TODO: var $volume = $(this).attr('data-v
+  $('#vider-element').each(function() {
+    var videBg = $(this).attr('data-vide-bg');
     $(this).vide(videBg);
-  }));
-}
+  });
+});
